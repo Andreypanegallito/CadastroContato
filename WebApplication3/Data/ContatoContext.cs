6b75553dd7d06890619461b1ebf05d067d02524a
Andrey Lucas Machado Panegalli <andreigd20@gmail.com>
2022-07-06 17:38:51 -0300

    Add files via upload

---
+++ WebApplication3/Data/ContatoContext.cs
@@ -0,0 +1,18 @@
+using WebApplication3.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
